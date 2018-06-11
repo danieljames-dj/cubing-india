2f307583cf05f0b7b98fdb2867fb2eb700b3d5f8
deb <debkbanerji@gmail.com>
2018-06-10 22:21:47 -0700

    Connecting to DB

--- services/database-connection.js
+++ services/database-connection.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const sequelize = require('sequelize');
 
 const exported = {};
 
@@ -7,10 +8,10 @@ let databaseCredentials;
 // Look for database credentials in file
 const databaseCredentialsFilePath = path.join('credentials', 'database-connection-credentials.json');
 if (fs.existsSync(databaseCredentialsFilePath)) {
-    databaseCredentials = fs.readFileSync(databaseCredentialsFilePath, 'utf8');
+    databaseCredentials = JSON.parse(fs.readFileSync(databaseCredentialsFilePath, 'utf8'));
 }
 
-if (process.env.DATABASE && process.env.DATABASE_USERNAME && process.env.DATABASE_PASSWORD) {
+if (process.env.DATABASE && process.env.DATABASE_USERNAME && process.env.DATABASE_PASSWORD && process.env.DATABASE_HOST) {
     databaseCredentials = {
         "database": process.env.DATABASE,
         "host": process.env.DATABASE_HOST,
@@ -25,9 +26,31 @@ if (!databaseCredentials) {
     // throw new Error(errorMessage);
 }
 
-// TODO: use databaseCredentials to connect to the database
+console.log(databaseCredentials);
 
-exported.get = function(table, request, callback) {
+const connection = new sequelize(databaseCredentials.database, databaseCredentials.username, databaseCredentials.password, {
+    host: databaseCredentials.host,
+    dialect: 'postgres', // Assuming Postgres for now since no one told me what type of database we're using
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+});
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Database connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+exported.get = function (table, request, callback) {
     callback("Nothing in database yet bro")
 };
 
